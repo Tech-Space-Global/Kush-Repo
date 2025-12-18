57202ba82912db8d4a97346566a586e79b8220e3
Lav Upadhyay <lavupadhayay@Lavs-MacBook-Air.local>
2025-12-18 13:50:59 +0530

    18 december

---
+++ Day1_Assignment.py
@@ -0,0 +1,12 @@
+#Q11. Develop a Python script to calculate the area of a rectangle. Your program should take the rectangle's length and width, and then display the calculated area.
+
+# length = float(input('enter length:'))
+# width = float(input('enter width:'))
+# area = length * width
+# print(f'area of rectangle is: {area}')
+
+#Q12. Write a Python program that converts temperatures from Celsius to Fahrenheit. Use the formula (Celsius * 9/5) + 32 to perform the conversion.
+
+celsius = float(input("enter the temperature in celsius"))
+fahrenheit = (celsius * 9/5) + 32
+print(f"temperature in fahrenheit: {fahrenheit}")
